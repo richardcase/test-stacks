bd210ed9752604155641c97d5755de168f10eae4
Richard Case <richard@weave.works>
2022-01-19 06:07:41 +0000

    third change

--- main.go
+++ main.go
@@ -5,8 +5,13 @@ import "fmt"
 func main() {
 	fmt.Println("This is the start of testing stacks")
 	printVersion()
+	printHello()
 }
 
 func printVersion() {
 	fmt.Println("v0.0.1")
+}
+
+func printHello() {
+	fmt.Println("hello there")
 }
